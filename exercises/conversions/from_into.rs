5a68e0d433716a95a541d060f99009b0912211b4
jlee96 <jlee96@foxmail.com>
2025-04-05 16:49:45 +0800

    完成 Clippy代码检查、From、TryFrom、FromStr、as、 AsRef、AsMut、unsafe、构建脚本、extern的练习

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,20 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let (name, age_str) = match s.split_once(",") {
+            Some((name, age_str)) if !name.trim().is_empty() => (name.trim(), age_str.trim()),
+            _ => return Person::default(),
+        };
+        if let Ok(age) = age_str.parse::<usize>() {
+            Person {
+                name: name.to_string(),
+                age,
+            }
+        } else {
+            Person::default()
+        }
     }
 }
 
